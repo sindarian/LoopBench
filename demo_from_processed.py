3b458dbc143c829894cf48ed68afb2384a04954d
sindarian <sindarian@users.noreply.github.com>
2025-07-12 11:53:41 -0400

    Cleaning out old metrics and plots

--- demo_from_processed.py
+++ demo_from_processed.py
@@ -1,15 +1,19 @@
+import os
+
 import plotting
+from metric_configs import resnet_hyperparams, efficient_net_hyperparams
 from sample_patches import run_sample_patches
 from generate_node_features import run_generate_node_features
 from predict import run_output_predictions
 import textwrap
-from train import train_run_cnn, train_efficient_net, train_attention_efficient_net, \
-    train_cbam_efficient_net
+from train import train_run_cnn, train_loop_net
 # from plotting import plot_metric
 from logger import Logger
 import logging
+import random
+import numpy as np
 
-from utils import PATCH_SIZE
+from utils import PATCH_SIZE, LOG_LEVEL, create_hic_generators, estimate_upper_bound
 
 LOGGER = Logger(name='demo_from_processed', level=logging.DEBUG).get_logger()
 
@@ -22,9 +26,6 @@ if __name__ == '__main__':
     # Define the unique ID for this run of experiment
     # (i.e. the unique name of the model trained in this experiment)
     run_id = 'demo'
-
-    # Random seed. Change this value if your model failed to converge
-    seed = 1024
 
     # Specify the genome assemblies (reference genome) of the source and target Hi-C/ChIA-PET
     # In this demo, source and target cell lines are sequenced with different assembly genomes
@@ -60,6 +61,7 @@ if __name__ == '__main__':
 
     # Define the chromosomes we draw training data from
     source_chroms = [str(i) for i in range(1, 23)] + ['X']
+
     # Define the chromosomes of the target cell line we want to predict on
     target_chroms = \
         [str(i) for i in range(1, 18)] + \
@@ -78,13 +80,7 @@ if __name__ == '__main__':
 
     # LOGGER.info('SAMPLING SOURCE CELL LINE - GM12878 w/ hg19')
     # Sample patches for source cell line
-    # run_sample_patches(dataset_name=source_dataset_name,
-    #                    assembly=source_assembly,
-    #                    bedpe_path=source_bedpe_path,
-    #                    image_txt_dir=source_image_data_dir,
-    #                    graph_txt_dir=source_graph_data_dir,
-    #                    chroms=source_chroms,
-    #                    patch_size=PATCH_SIZE)
+    # run_sample_patches(dataz=PATCH_SIZE)
     # Generate the node features for source cell line
     # run_generate_node_features(source_dataset_name, source_chroms, source_assembly)
 
@@ -96,15 +92,37 @@ if __name__ == '__main__':
     #     target_bedpe_path,
     #     target_image_data_dir,
     #     target_graph_data_dir,
-    #     target_chroms)
+    #     target_chroms,
+    #     PATCH_SIZE)
     # Generate the node features for target cell line
     # run_generate_node_features(target_dataset_name, target_chroms, target_assembly)
 
+    LOGGER.info('Create Generators for Train, Val, and Test Data')
+    train_gen, val_gen, test_gen = create_hic_generators(
+        chrom_names=source_chroms,
+        data_dir='dataset/'+source_dataset_name,
+        patch_size=PATCH_SIZE,
+        split_ratios=(0.8, 0.1, 0.1), # train, val, test
+        batch_size=8
+    )
+
+    x_train_upper_bound = estimate_upper_bound(train_gen, percentile=0.996)
+    print("99.6% upper bound:", x_train_upper_bound)
+
     LOGGER.info('Training the U-net models')
-    # train_run_cnn(source_chroms, run_id, seed, source_dataset_name, epochs=50)
-    train_efficient_net(source_chroms, seed, source_dataset_name, epochs=50)
-    train_cbam_efficient_net(source_chroms, seed, source_dataset_name, epochs=50)
-    train_attention_efficient_net(source_chroms, seed, source_dataset_name, epochs=50)
+    train_run_cnn(train_gen, val_gen, test_gen, clip_value=x_train_upper_bound, epochs=1)
+    # train_loop_net(chroms=source_chroms,
+    #                dataset_name=source_dataset_name,
+    #                model_name='efnb0',
+    #                hyper_params=efficient_net_hyperparams,
+    #                epochs=50,
+    #                batch_size=8)
+    train_loop_net(dataset_name=source_dataset_name,
+                   train_gen=train_gen,
+                   val_gen=val_gen,
+                   test_gen=test_gen,
+                   clip_value=x_train_upper_bound,
+                   epochs=1)
 
     # LOGGER.info('Testing the U-net models')
     # Predict on the target cell line
